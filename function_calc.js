a832869e2c5eb140175d1e4036027a9d9a37809e
DAVRAN <davran-mashrabov30@yandex.ru>
2022-01-22 19:58:09 +0300

    second commit

--- function_calc.js
+++ function_calc.js
@@ -23,35 +23,3 @@ function calculator(operator, a = 'string', b = 'string') {
 	}
 }
 console.log(calculator('minus', '552', 2));
-// "use strict";
-// let operator;
-// let a;
-// let b;
-// let checkA;
-// let checkB;
-// function calculator(operator, a = 'string', b = 'string') {
-// 	checkA = typeof (a);
-// 	checkB = typeof (b);
-// 	switch (checkA, checkB) {
-// 		case 'string':
-// 			return 'Error';
-// 		default:
-// 			switch (operator) {
-// 				case 'summ':
-// 					return a + b;
-// 				case 'minus':
-// 					return a - b;
-// 				case 'divide':
-// 					return a / b;
-// 				case 'multiply':
-// 					return a * b;
-// 				case 'remainder':
-// 					return a % b;
-// 				case 'pow':
-// 					return a ** b;
-// 				default:
-// 					return 'unknown operation'
-// 			}
-// 	}
-// }
-// console.log(calculator('minus', '552', 2));
